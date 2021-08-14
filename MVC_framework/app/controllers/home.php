d3ba3d416b128aa31773a3a065ce9d789f61ca3a
sanjana-sabah-khan <79992325+sanjana-sabah-khan@users.noreply.github.com>
2021-08-15 00:03:28 +0600

    Update home.php

--- MVC_framework/app/controllers/home.php
+++ MVC_framework/app/controllers/home.php
@@ -1,1 +1,18 @@
-
+<?php
+    
+    class Home extends Controller {
+        
+    
+        public function __construct () {
+            echo "home Loaded";
+        }
+        
+        public function index ($name = '') {
+            $user = $this->model('model1');
+            $user->name = 'Model1';
+            
+            $this->view('view1', ['name' => $user->name]);
+        }
+        
+        
+    }
